2c68cd163f3c41ae449da813b9d3d9384dff0a37
Patryk Zioło <patryk.ziolo12@gmail.com>
2024-09-30 15:38:49 +0200

    app file

--- library_app/__init__.py
+++ library_app/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 
-@app.route('/')
-def home():
-    return '<h1> Test </h1>'
-    
-    
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+
+from library_app import authors
+from library_app import models
