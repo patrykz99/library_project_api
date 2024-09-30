0858a56ebd19f0e827ffed431f861eb3a00eff8e
Patryk Zioło <patryk.ziolo12@gmail.com>
2024-09-30 15:36:41 +0200

    Config file

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 from pathlib import Path
-import os
+import os 
 
 app_dir = Path(__file__).resolve().parent
 env_file = app_dir / '.env'
@@ -9,5 +9,8 @@ env_file = app_dir / '.env'
 load_dotenv(env_file)
 
 class Config:
-    DEBUG = True
+    FLASK_DEBUG = os.environ.get('FLASK_DEBUG')
     SECRET_KEY = os.environ.get('SECRET_KEY') #key to security
+    FLASK_APP = os.environ.get('FLASK_APP')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
