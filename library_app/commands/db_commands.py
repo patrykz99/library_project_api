4278b6abca4497f7f810bb8861a8e1fee61ea00d
Patryk Zioło <patryk.ziolo12@gmail.com>
2024-10-15 17:29:38 +0200

    Added to db commands function to load json, modified add-data and remove-data

--- library_app/commands/db_commands.py
+++ library_app/commands/db_commands.py
@@ -1,5 +1,5 @@
 from library_app import db
-from library_app.models import Author
+from library_app.models import Author,Book
 from sqlalchemy import text
 from pathlib import Path
 from datetime import datetime
@@ -7,6 +7,15 @@ import json
 from library_app.commands import db_commands_blueprint
 
 
+def load_json():
+    json_path_authors = Path(__file__).parent.parent /'data'/'authors.json'
+    json_path_books = Path(__file__).parent.parent / 'data'/'books.json'
+    with open(json_path_authors) as file:
+        data_authors = json.load(file)
+    with open(json_path_books) as file:
+        data_books = json.load(file)
+    return (data_authors,data_books)
+            
 @db_commands_blueprint.cli.group()
 def db_commands():
     '''Commands to manipulate database'''
@@ -16,13 +25,14 @@ def db_commands():
 def add_data():
     '''Add all data from file (JSON) to the table'''
     try:
-        json_path = Path(__file__).parent.parent /'data'/'authors.json'
-        with open(json_path) as file:
-            data = json.load(file)
-        for e in data:
+        data = load_json()   
+        for e in data[0]:
             e['date_of_birth'] = datetime.strptime(e['date_of_birth'],"%d-%m-%Y").date()
             author = Author(**e)
             db.session.add(author)
+        for e in data[1]:
+            book = Book(**e)
+            db.session.add(book)
         db.session.commit() 
         print('Data has been added succesfully')  
     except Exception as err:
@@ -32,9 +42,10 @@ def add_data():
 def remove_data():
     '''Remove all data from the table'''
     try:
-        
-        db.session.execute(text(f'TRUNCATE TABLE {Author.__tablename__}'))
-        db.session.execute(text(f"ALTER SEQUENCE {Author.__tablename__}_id_seq RESTART WITH 1;")) #Alternatively TRUNCATE TABLE {Author.__tablename__} RESTART IDENTITY;
+        db.session.execute(text(f'DELETE FROM {Book.__tablename__};'))
+        db.session.execute(text(f'ALTER SEQUENCE {Book.__tablename__}_id_seq RESTART WITH 1;'))
+        db.session.execute(text(f'DELETE FROM {Author.__tablename__};'))
+        db.session.execute(text(f'ALTER SEQUENCE {Author.__tablename__}_id_seq RESTART WITH 1;'))
         db.session.commit()
         print('Data has been removed succesfully') 
     except Exception as err:
