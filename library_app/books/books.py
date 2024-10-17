5ebb6eb340b2413d431a66a08a535996108bbcfd
Patryk Zioło <patryk.ziolo12@gmail.com>
2024-10-17 03:35:51 +0200

    Added route for get single book

--- library_app/books/books.py
+++ library_app/books/books.py
@@ -1,6 +1,6 @@
 from library_app import db
 from flask import jsonify,request
-from library_app.models import Book,Book_Schema
+from library_app.models import Book,Book_Schema,book_schema
 from webargs.flaskparser import use_args
 from library_app.utils import validate_content_type_json,get_schema_params,sort_data,filter_data,make_pagination
 from library_app.books import books_blueprint
@@ -18,4 +18,12 @@ def get_books():
         'data': author_schema_list.dump(items_page),
         'amount':len(items_page),
         'pagination': pagination
+    })
+    
+@books_blueprint.route('/books/<int:book_id>',methods=['GET'])
+def get_book(book_id):
+    book = Book.query.get_or_404(book_id,description=f'No book with id {book_id} in the database')
+    return jsonify({
+        'success':True,
+        'data': book_schema.dump(book)
     })
